c2503b020808a21c0c5e4c4549560a8e0f65491c
AyushInKC <sirfayush1008@gmail.com>
2024-12-05 09:15:39 +0530

    Project Setup

---
+++ src/main/java/com/JavaJunkie/OTP/Verifier/VerifyByOtpApplication.java
@@ -0,0 +1,13 @@
+package com.JavaJunkie.OTP.Verifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VerifyByOtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VerifyByOtpApplication.class, args);
+	}
+
+}
